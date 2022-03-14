321e851cf2f1f2450b00dc105a6fca09aa73bf9d
yimsoijoi <p.winaitammakul@gmail.com>
2022-03-14 08:25:22 +0700

    writing worker in dns package

--- dnsserver/server.go
+++ dnsserver/server.go
@@ -5,58 +5,100 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/likexian/doh-go"
+	dohdns "github.com/likexian/doh-go/dns"
 	"github.com/miekg/dns"
 	"github.com/pkg/errors"
 	"github.com/yimsoijoi/stubresolver/cacher"
+	"github.com/yimsoijoi/stubresolver/dohclient"
 )
-
-var records map[string]string
 
 type Server struct {
 	Ctx       context.Context
 	DnsServer dns.Server
-	DohClient doh.DoH
-	Cacher     cacher.Cacher
+	DohClient dohclient.DohCli
+	Cacher    cacher.Cacher
 }
 
 type answerMap map[cacher.Key][]string
 
-func(s *Server) parseQuery(m *dns.Msg) error {
+// func (s *Server) RR(dName, t, v string) (dns.RR, error) {
+// 	return dns.NewRR(fmt.Sprintf("%s %s %d", dName, t, v))
+// }
+
+func (s *Server) Worker(m *dns.Msg) error {
 	for _, q := range m.Question {
 		switch q.Qtype {
 		case dns.TypeA:
-			key := cacher.NewKey(q.Name,q.Qtype,-1)
-			ip, err := s.Cacher.Get(key)
-			if ip != nil && err == nil {
+			log.Printf("query for %s (TypeA)\n ", q.Name)
+			k := cacher.NewKey(q.Name, "A", -1)
+			ip, err := s.Cacher.Get(k)
+			if err == nil {
 				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
-				if err == nil {
-					m.Answer = append(m.Answer, rr)		t, ok := dnsTypes[q.Qtype]
-			if !ok {
-				fmt.Errorf("unsupported DNS type:%v",q.Qtype)
-			}
-			key := cacher.NewKey(q.Name,q.Qtype,-1)
-			ip, err := s.Cacher.Get(key)
-			if ip != nil && err == nil {
-				rr, err := dns.NewRR(q.Name,t, ip)
 				if err == nil {
 					m.Answer = append(m.Answer, rr)
 				}
+				return err
 			}
-		
+		case dns.TypeAAAA:
+			log.Printf("query for %s (TypeAAAA)\n ", q.Name)
+			k := cacher.NewKey(q.Name, "AAAA", -1)
+			ip, err := s.Cacher.Get(k)
+			if err == nil {
+				rr, err := dns.NewRR(fmt.Sprintf("%s AAAA %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+				return err
+			}
+		default:
+			log.Println("DoH query")
+			dom := dohdns.Domain(q.Name)
+			dohAnswers, err := s.DohClient.GetAnswer(s.Ctx, dom)
+			if err != nil {
+				return errors.Wrap(err, fmt.Sprintf("failed to get DoH answer for %s", dom))
+			}
+			for _, ans := range dohAnswers {
+				switch ans.Type {
+				case int(dns.TypeA):
+					log.Printf("query for %s (TypeA)\n ", q.Name)
+					k := cacher.NewKey(q.Name, "A", -1)
+					ip, err := s.Cacher.Get(k)
+					if err == nil {
+						rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+						if err == nil {
+							m.Answer = append(m.Answer, rr)
+						}
+						return err
+					}
+				case int(dns.TypeAAAA):
+					log.Printf("query for %s (TypeAAAA)\n ", q.Name)
+					k := cacher.NewKey(q.Name, "AAAA", -1)
+					ip, err := s.Cacher.Get(k)
+					if err == nil {
+						rr, err := dns.NewRR(fmt.Sprintf("%s AAAA %s", q.Name, ip))
+						if err == nil {
+							m.Answer = append(m.Answer, rr)
+						}
+						return err
+					}
+				}
+
+			}
+
+		}
 	}
 	return nil
 }
 
-func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
-	m := new(dns.Msg)
-	m.SetReply(r)
-	m.Compress = false
+// // func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+// 	m := new(dns.Msg)
+// 	m.SetReply(r)
+// 	m.Compress = false
 
-	switch r.Opcode {
-	case dns.OpcodeQuery:
-		parseQuery(m)
-	}
+// 	switch r.Opcode {
+// 	case dns.OpcodeQuery:
+// 		parseQuery(m)
+// 	}
 
-	w.WriteMsg(m)
-}
+// 	w.WriteMsg(m)
+// }
